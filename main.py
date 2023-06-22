df2c7f95f5d9fd853903c506d0d012d6f17f36d6
CHEN LUDA <45784833+chenluda@users.noreply.github.com>
2023-06-22 11:09:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -233,4 +233,4 @@ if __name__ == "__main__":
     # url = "https://www.zhihu.com/column/c_1649842617335672832"
 
     # hexo_uploader=True 表示在公式前后加上 {% raw %} {% endraw %}，以便 hexo 正确解析
-    judge_zhihu_type(url, hexo_uploader=True)
+    judge_zhihu_type(url, hexo_uploader=False)
